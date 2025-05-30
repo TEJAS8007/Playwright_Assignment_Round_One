fba1acc97a587132dbf1905829b1af9762b5c3f3
TEJAS8007 <awaretejas55@gmail.com>
2025-05-30 21:27:36 +0530

    Second Commit

--- tests/Test_Folder/Login_Test.spec.js
+++ tests/Test_Folder/Login_Test.spec.js
@@ -2,19 +2,24 @@ import { test, expect } from '@playwright/test';
 import data from '../Utilities/config.json';
 import log from '../Utilities/logger';
 import { Login_Page } from '../Pages/Login_Page';
+import path from 'path';
 
 let page;
 let login;
 let context;
 
 test.beforeAll(async ({ browser }) => {
-    context = await browser.newContext();
+    context = await browser.newContext({
+        recordVideo: { dir: 'videos/' } 
+    });
     page = await context.newPage();
     login = new Login_Page(page);
-    log.debug('Before Each Started....');
+    log.debug('Before All Started....');
 });
 
+
 test('Login_Test', async () => {
+    await page.context().tracing.start({screenshots:true,snapshots:true});
     log.info('Get page Started....');
     await login.get_Login_Page(data.url);
     log.info('Get page Ended....');
@@ -24,9 +29,11 @@ test('Login_Test', async () => {
     log.info('Login Action Started....');
     await login.complete_Login_Action(data.un, data.ps);
     log.info('Login Action Ended....');
+    await page.context().tracing.stop({path:'Login.zip'});
 });
 
 test.afterAll(async () => {
+    
     await page.close();
     await context.close();
     log.info('Page closed......');
